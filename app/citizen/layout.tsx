93787ca8498fbf14f605f6932b0eb351e9ad7303
Wistant Kode <wistantkode@protonmail.com>
2026-07-01 03:36:45 +0100

    style(citizen): update background to pattern of multiple rotated Cameroon Crests

--- app/citizen/layout.tsx
+++ app/citizen/layout.tsx
@@ -8,17 +8,20 @@ export default function CitizenLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen bg-neutral-50/30 flex flex-col font-sans antialiased text-neutral-800 relative">
-      {/* Background Cameroon Crest Watermark */}
-      <div className="fixed inset-0 pointer-events-none opacity-[0.025] flex items-center justify-center z-0 select-none">
-        <div className="relative w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] md:w-[700px] md:h-[700px]">
-          <Image
-            src="/cameroon-logo.png"
-            alt="Armoiries du Cameroun"
-            fill
-            className="object-contain"
-            priority
-          />
+    <div className="min-h-screen bg-neutral-50/30 flex flex-col font-sans antialiased text-neutral-800 relative overflow-x-hidden">
+      {/* Background Cameroon Crest Watermarks Pattern - Inclined and visible */}
+      <div className="fixed inset-0 pointer-events-none opacity-[0.035] z-0 select-none overflow-hidden">
+        <div className="absolute top-12 left-10 w-44 h-44 -rotate-[22deg]">
+          <Image src="/cameroon-logo.png" alt="Armoiries du Cameroun" fill className="object-contain" />
+        </div>
+        <div className="absolute top-1/4 right-12 w-52 h-52 rotate-[18deg]">
+          <Image src="/cameroon-logo.png" alt="Armoiries du Cameroun" fill className="object-contain" />
+        </div>
+        <div className="absolute bottom-1/3 left-16 w-48 h-48 -rotate-[15deg]">
+          <Image src="/cameroon-logo.png" alt="Armoiries du Cameroun" fill className="object-contain" />
+        </div>
+        <div className="absolute bottom-12 right-20 w-56 h-56 rotate-[25deg]">
+          <Image src="/cameroon-logo.png" alt="Armoiries du Cameroun" fill className="object-contain" />
         </div>
       </div>
       {/* Cameroonian National Colors Ribbon */}
